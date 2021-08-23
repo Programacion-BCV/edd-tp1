dc5167c9fe83a77e1c9a09eff1d51c2b84c8b3c9
matiassvera <matiassvera18@gmail.com>
2021-08-23 13:01:43 -0300

    elimine import array que no se usaba

--- src/main/java/com/programacion_bcd/ejercicio4/ClientesAutomoviles.java
+++ src/main/java/com/programacion_bcd/ejercicio4/ClientesAutomoviles.java
@@ -1,38 +1,38 @@
 package com.programacion_bcd.ejercicio4;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
 
 public class ClientesAutomoviles {
+
     public static void main(String[] args) {
         Automovil[] param = new Automovil[4];
 
-        param[0] = new Automovil("abc 478", new Tipo(("Sedan")), new Marca(("Fiat")), 2010,new BigDecimal("300000"));
+        param[0] = new Automovil("abc 478", new Tipo(("Sedan")), new Marca(("Fiat")), 2010, new BigDecimal("300000"));
 
-        param[1] = new Automovil("abc 693", new Tipo(("Berlina")), new Marca(("Peugeot")),2019,new BigDecimal("2000000"));
+        param[1] = new Automovil("abc 693", new Tipo(("Berlina")), new Marca(("Peugeot")), 2019, new BigDecimal("2000000"));
 
-        param[2] = new Automovil("abcd 1248", new Tipo(("Camioneta")), new Marca(("Toyota")), 2019,new BigDecimal("5000000"));
+        param[2] = new Automovil("abcd 1248", new Tipo(("Camioneta")), new Marca(("Toyota")), 2019, new BigDecimal("5000000"));
 
-        param[3] = new Automovil("abc 123", new Tipo(("Sedan")), new Marca(("Volkswagen")),2010,new BigDecimal("500000"));
+        param[3] = new Automovil("abc 123", new Tipo(("Sedan")), new Marca(("Volkswagen")), 2010, new BigDecimal("500000"));
 
         System.out.println("Datos de Automoviles");
         imprimir(param);
-        
+
         System.out.println("");
 
         System.out.println("Cantidad de vehiculos de igual modelo:");
         imprimir(filtrarPorAnio(param, 2019));
-        
+
         System.out.println("");
 
         System.out.println("Automoviles de igual tipo:");
-        
+
         imprimir(filtrarPorTipo(param, new Tipo("Sedan")));
-        
+
         System.out.println("");
-        
+
         System.out.println("Cantidad de vehiculos por modelos:");
-        
+
         AnioCantidad[] retorno = cantidadPorAnio(param);
         for (int i = 0; i < retorno.length; i++) {
             if (retorno[i] != null) {
@@ -43,11 +43,11 @@ public class ClientesAutomoviles {
             }
         }
         System.out.println("");
-        
+
         System.out.println("Sumatoria de valuaciones clasificadas por tipo de vehiculo");
-        
+
         TipoValuacion[] retorno1 = valuacionesPorTipo(param);
-        
+
         for (int i = 0; i < retorno1.length; i++) {
             if (retorno1[i] != null) {
                 System.out.println(retorno1[i].toString());
@@ -56,7 +56,6 @@ public class ClientesAutomoviles {
                 break;
             }
         }
-        
 
     }
 
@@ -120,7 +119,7 @@ public class ClientesAutomoviles {
         return retorno;
     }
 
-private static TipoValuacion[] valuacionesPorTipo(Automovil[] param) {
+    private static TipoValuacion[] valuacionesPorTipo(Automovil[] param) {
         int j;
         boolean pusheado;
         TipoValuacion[] retorno1 = new TipoValuacion[param.length];
@@ -128,7 +127,7 @@ private static TipoValuacion[] valuacionesPorTipo(Automovil[] param) {
         for (int i = 0; i < param.length; i++) {
             pusheado = false;
             if (i == 0) {
-                retorno1[i] = new TipoValuacion(param[i].getTipo().getDescripcion(),param[i].getValuacion());
+                retorno1[i] = new TipoValuacion(param[i].getTipo().getDescripcion(), param[i].getValuacion());
             } else {
                 j = 0;
 
@@ -149,4 +148,4 @@ private static TipoValuacion[] valuacionesPorTipo(Automovil[] param) {
         }
         return retorno1;
     }
-} 
+}
